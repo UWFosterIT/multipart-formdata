0ed24d665252df6f38f199132bd9b0baf2828889
Steven Chadwick <schad@uw.edu>
2018-01-12 10:03:57 -0800

    Updates to code

--- multipart.js
+++ multipart.js
@@ -1,138 +1,84 @@
-/**
- 	Multipart Parser (Finite State Machine)
+exports.parse = (multipartBuffer, boundary) => {
 
-	usage:
+  let process = file => {
+    let headers     = file.header.split(';');
+    let fileHeader  = headers[2].split('=');
+    let filename    = JSON.parse(fileHeader[1].trim());
+    let contentType = file.info.split(':')[1].trim();
+    let data        = new Buffer(file.part);
 
-	var multipart = require('./multipart.js');
-	var body = multipart.DemoData(); 							   // raw body
-	var body = new Buffer(event['body-json'].toString(),'base64'); // AWS case
-	
-	var boundary = multipart.getBoundary(event.params.header['content-type']);
-	var parts = multipart.Parse(body,boundary);
-	
-	// each part is:
-	// { filename: 'A.txt', type: 'text/plain', data: <Buffer 41 41 41 41 42 42 42 42> }
+    return {
+      data:     data,
+      filename: filename,
+      type:     contentType
+    };
+  };
 
-	author:  Cristian Salazar (christiansalazarh@gmail.com) www.chileshift.cl
-			 Twitter: @AmazonAwsChile
- */
-exports.Parse = function(multipartBodyBuffer,boundary){
-	var process = function(part){
-		// will transform this object:
-		// { header: 'Content-Disposition: form-data; name="uploads[]"; filename="A.txt"',
-		//	 info: 'Content-Type: text/plain',
-		//	 part: 'AAAABBBB' }
-		// into this one:
-		// { filename: 'A.txt', type: 'text/plain', data: <Buffer 41 41 41 41 42 42 42 42> }
-		var obj = function(str){
-			var k = str.split('=');
-			var a = k[0].trim();
-			var b = JSON.parse(k[1].trim());
-			var o = {};
-			Object.defineProperty( o , a , 
-			{ value: b, writable: true, enumerable: true, configurable: true })
-			return o;
-		}
-		var header = part.header.split(';');		
-		var file = obj(header[2]);
-		var contentType = part.info.split(':')[1].trim();		
-		Object.defineProperty( file , 'type' , 
-			{ value: contentType, writable: true, enumerable: true, configurable: true })
-		Object.defineProperty( file , 'data' , 
-			{ value: new Buffer(part.part), writable: true, enumerable: true, configurable: true })
-		return file;
-	}
-	var prev = null;
-	var lastline='';
-	var header = '';
-	var info = ''; var state=0; var buffer=[];
-	var allParts = [];
+  let lastline = '';
+  let header   = '';
+  let info     = '';
+  let state    = 0;
+  let buffer   = [];
+  let files    = [];
 
-	for(i=0;i<multipartBodyBuffer.length;i++){
-		var oneByte = multipartBodyBuffer[i];
-		var prevByte = i > 0 ? multipartBodyBuffer[i-1] : null;
-		var newLineDetected = ((oneByte == 0x0a) && (prevByte == 0x0d)) ? true : false;
-		var newLineChar = ((oneByte == 0x0a) || (oneByte == 0x0d)) ? true : false;
+  for (let i = 0; i < multipartBuffer.length; i++) {
+    let currByte        = multipartBuffer[i];
+    let prevByte        = i > 0 ? multipartBuffer[i - 1] : null;
+    let newLineDetected = (currByte === 0x0a) && (prevByte === 0x0d);
+    let newLineChar     = (currByte === 0x0a) || (currByte === 0x0d);
 
-		if(!newLineChar)
-			lastline += String.fromCharCode(oneByte);
+    if (!newLineChar) {
+      lastline += String.fromCharCode(currByte);
+    }
 
-		if((0 == state) && newLineDetected){
-			if(("--"+boundary) == lastline){
-				state=1;
-			}
-			lastline='';
-		}else
-		if((1 == state) && newLineDetected){
-			header = lastline;
-			state=2;
-			lastline='';
-		}else
-		if((2 == state) && newLineDetected){
-			info = lastline;
-			state=3;
-			lastline='';
-		}else
-		if((3 == state) && newLineDetected){
-			state=4;
-			buffer=[];
-			lastline='';
-		}else
-		if(4 == state){
-			if(lastline.length > (boundary.length+4)) lastline=''; // mem save
-			if(((("--"+boundary) == lastline))){
-				var j = buffer.length - lastline.length;
-				var part = buffer.slice(0,j-1);
-				var p = { header : header , info : info , part : part  };
-				allParts.push(process(p));
-				buffer = []; lastline=''; state=5; header=''; info='';
-			}else{
-				buffer.push(oneByte);
-			}
-			if(newLineDetected) lastline='';
-		}else
-		if(5==state){
-			if(newLineDetected)
-				state=1;
-		}
-	}
-	return allParts;
+    if ((0 === state) && newLineDetected) {
+      if (`--${boundary}` === lastline) {
+        state = 1;
+      }
+      lastline = '';
+    } else if ((1 === state) && newLineDetected) {
+      header   = lastline;
+      lastline = '';
+      state    = 2;
+    } else if ((2 === state) && newLineDetected) {
+      info     = lastline;
+      lastline = '';
+      state    = 3;
+    } else if ((3 === state) && newLineDetected) {
+      buffer   = [];
+      lastline = '';
+      state    = 4;
+    } else if (4 === state) {
+      if (lastline.length > (boundary.length + 4)) {
+        lastline = '';
+      }
+
+      if (`--${boundary}` === lastline) {
+        let j = buffer.length - lastline.length;
+        let part = buffer.slice(0, j - 1);
+
+        files.push(process({
+          header: header,
+          info:   info,
+          part:   part
+        }));
+
+        buffer   = [];
+        header   = '';
+        info     = '';
+        lastline = '';
+        state    = 5;
+      } else {
+        buffer.push(currByte);
+      }
+      if (newLineDetected) {
+        lastline = '';
+      }
+    } else if (5 === state && newLineDetected) {
+      state = 1;
+    }
+  }
+  return files;
 };
 
-//  read the boundary from the content-type header sent by the http client
-//  this value may be similar to:
-//  'multipart/form-data; boundary=----WebKitFormBoundaryvm5A9tzU1ONaGP5B',
-exports.getBoundary = function(header){
-	var items = header.split(';');
-	if(items)
-		for(i=0;i<items.length;i++){
-			var item = (new String(items[i])).trim();
-			if(item.indexOf('boundary') >= 0){
-				var k = item.split('=');
-				return (new String(k[1])).trim();
-			}
-		}
-	return "";
-}
-
-exports.DemoData = function(){
-	body = "trash1\r\n"
-	body += "------WebKitFormBoundaryvef1fLxmoUdYZWXp\r\n";
-	body += "Content-Disposition: form-data; name=\"uploads[]\"; filename=\"A.txt\"\r\n";
-	body += "Content-Type: text/plain\r\n",
-	body += "\r\n\r\n";
-	body += "@11X";
-	body += "111Y\r\n";
-	body += "111Z\rCCCC\nCCCC\r\nCCCCC@\r\n\r\n";
-	body += "------WebKitFormBoundaryvef1fLxmoUdYZWXp\r\n";
-	body += "Content-Disposition: form-data; name=\"uploads[]\"; filename=\"B.txt\"\r\n";
-	body += "Content-Type: text/plain\r\n",
-	body += "\r\n\r\n";
-	body += "@22X";
-	body += "222Y\r\n";
-	body += "222Z\r222W\n2220\r\n666@\r\n";
-	body += "------WebKitFormBoundaryvef1fLxmoUdYZWXp--\r\n";
-	return (new Buffer(body,'utf-8')); 
-	// returns a Buffered payload, so the it will be treated as a binary content.
-};
-
+exports.getBoundary = header => header.split('; boundary=')[1];
---
+++ test/upload-spec.js
@@ -0,0 +1,35 @@
+let chai      = require('chai');
+let multipart = require('../multipart');
+let fs        = require('fs');
+
+const expect = chai.expect;
+
+describe('Upload tests', () => {
+
+  it('should accept jpeg images', () => {
+    let filename = 'test.txt';
+    let boundary = '--------------------------726963471261442896443806';
+
+    let body = `--${boundary}\r\n`;
+    body += `Content-Disposition: form-data; name="image"; filename="${filename}"\r\n`;
+    body += 'Content-Type: text/plain\r\n';
+    body += '\r\n';
+    body += fs.readFileSync(`test/${filename}`);
+    body += '\r\n';
+    body += `--${boundary}--\r\n`;
+
+    let req = {
+      body:    new Buffer(body),
+      headers: {'Content-Type': `multipart/form-data; boundary=${boundary}`}
+    };
+
+    let getBoundary = multipart.getBoundary(req.headers['Content-Type']);
+    let parts = multipart.parse(req.body, getBoundary);
+
+    expect(Array.isArray(parts)).to.eql(true);
+    expect(parts.length).to.eql(1);
+    expect(parts[0].filename).to.eql(filename);
+    expect(parts[0].type).to.eql('text/plain');
+  });
+
+});
